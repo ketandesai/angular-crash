0b87d0412a9a34a1b75a471c1fce02f11c0e3a00
ketandesai <desai.ketan@gmail.com>
2021-12-16 16:23:17 -0500

    initial commit

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {Task} from '../Task';
+import {Observable} from 'rxjs';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private apiUrl = 'http://localhost:3000/tasks';
+
+  constructor(private http:HttpClient) { }
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(url);
+  }
+
+  updateTaskReminder(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.put<Task>(url, task, httpOptions);
+  }
+}
